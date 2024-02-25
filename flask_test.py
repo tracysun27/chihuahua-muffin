2dbc5c118e5b6b909e6f93a493e86f41cd4e45ae
Tracy Sun <96366527+tracysun27@users.noreply.github.com>
2024-02-25 15:22:51 -0800

    Update flask_test.py

--- flask_test.py
+++ flask_test.py
@@ -13,9 +13,8 @@
 # def home():
 #     return render_template("home.html")
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, Response
 import cv2
-import os
 import datetime as dt
 
 app = Flask(__name__)
@@ -30,6 +29,19 @@ def capture_image():
     cv2.destroyAllWindows()
     return img_name
 
+# Function to generate frames from webcam feed
+def generate_frames():
+    vid = cv2.VideoCapture(0)
+    while True:
+        ret, frame = vid.read()
+        if not ret:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 @app.route('/')
 def home():
     return render_template("home.html")
@@ -38,6 +50,10 @@ def home():
 def capture():
     img_name = capture_image()
     return f"Image saved as {img_name}"
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True)
