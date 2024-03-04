0690ea30440816d471d31daa29594b53d6448289
Tracy Sun <96366527+tracysun27@users.noreply.github.com>
2024-03-04 14:16:48 -0800

    Show image and redirect to results page

--- flask_test.py
+++ flask_test.py
@@ -3,7 +3,7 @@
 # export FLASK_ENV=development
 # flask run
 
-from flask import Flask, render_template, Response, session, redirect, url_for
+from flask import Flask, render_template, Response, session, redirect, url_for, request
 from PIL import Image
 import os
 import cv2
@@ -11,40 +11,51 @@ import datetime as dt
 import numpy as np
 import tensorflow as tf
 
-app = Flask(__name__, root_path="/Users/trac.k.y/Downloads/chihuahua_muffin/app", static_folder=os.path.join("/Users/trac.k.y/Downloads/chihuahua_muffin/app", 'static'))
+app = Flask(__name__)
 app.secret_key = "509d5ad1fb502054034e60c79aa439b3"
-print("App Root Path:", app.root_path)
-print("Static Folder Path:", app.static_folder)
+# print("App Root Path:", app.root_path)
+# print("Static Folder Path:", app.static_folder)
 
-# # old code: save in desktop folder named "images"
-# try:
-#     os.mkdir('./images')
-# except OSError as error:
-#     pass
+# version 1: save in desktop folder named "images"
+try:
+    os.mkdir('./app/static')
+except OSError as error:
+    pass
 
-# uploaded Images folder path
-app.config['UPLOAD_FOLDER'] = './static'
-# Check if the folder directory exists, if not then create it
-if not os.path.exists(app.config['UPLOAD_FOLDER'] ):
-    os.makedirs(app.config['UPLOAD_FOLDER'])
+# # version 2: Images folder path
+# app.config['UPLOAD_FOLDER'] = './static'
+# # Check if the folder directory exists, if not then create it
+# if not os.path.exists(app.config['UPLOAD_FOLDER'] ):
+#     os.makedirs(app.config['UPLOAD_FOLDER'])
 
 # Function to capture and save an image
 def capture_image():
     vid = cv2.VideoCapture(0) 
     ret, frame = vid.read()
     img_name = f'captured_image_{dt.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.jpg'
-    #cv2.imwrite(f"./images/{img_name}", frame)
-
-    img_path = os.path.join(app.config['UPLOAD_FOLDER'], img_name)
-    
-    #img_path = f"./images/{img_name}"
+    img_path = f"./app/static/{img_name}"
     cv2.imwrite(img_path, frame)
-
     vid.release() 
     cv2.destroyAllWindows()
-
-    session['img_path'] = img_path
     return img_name, img_path
+
+# Function to capture and save an image
+# def capture_image():
+#     vid = cv2.VideoCapture(0) 
+#     ret, frame = vid.read()
+#     img_name = f'captured_image_{dt.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.jpg'
+#     #cv2.imwrite(f"./images/{img_name}", frame)
+
+#     img_path = os.path.join(app.config['UPLOAD_FOLDER'], img_name)
+    
+#     #img_path = f"./images/{img_name}"
+#     cv2.imwrite(img_path, frame)
+
+#     vid.release() 
+#     cv2.destroyAllWindows()
+
+#     session['img_path'] = img_path
+#     return img_name, img_path
 
 # function for converting captured image into correct dimensions for model
 def preprocess_image(image_path):
@@ -84,10 +95,10 @@ def home():
 def capture():
     #img_name = capture_image()
     #return f"Image saved as {img_name}"
-
     img_name, img_path = capture_image()
-
-    return render_template("capture.html", img_name=img_name, img_path=img_path)
+    session["img_path"] = img_path
+    return redirect(url_for('results', img_name=img_name, img_path=img_path))
+    # return render_template("capture.html", img_name=img_name, img_path=img_path)
 
 @app.route('/video_feed')
 def video_feed():
@@ -97,7 +108,8 @@ def video_feed():
 def results():
     img_path = session.get('img_path', None)
     res = run_model(img_path)
-    return render_template("results.html", img_path=img_path, res=res)
+    img_name = request.args.get('img_name')  
+    return render_template("results.html", img_path=img_path, img_name=img_name, res=res)
 
 if __name__ == '__main__':
     app.run(debug=True)
